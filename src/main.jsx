be270ac94ffe4cf83f8677121325f73ac86071a4
Agustin Romero <agusromero0815@gmail.com>
2023-03-15 15:20:58 -0300

    eliminando react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import { MyProvider } from "./GlobalContext/GlobalContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <MyProvider>
-        <App />
-      </MyProvider>
-    </BrowserRouter>
+    <MyProvider>
+      <App />
+    </MyProvider>
   </React.StrictMode>
 );
